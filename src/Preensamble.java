5fb07400b13fb501818f5dbbca5d08c1bfecfbb0
Robii Aragon <55045932+RobiiAragon@users.noreply.github.com>
2024-04-06 21:39:51 -0700

    Cambios
    ahora se va sumando el subtotal en el apartado de Preensamble

--- src/Preensamble.java
+++ src/Preensamble.java
@@ -1,39 +1,42 @@
 import java.util.Scanner;
 
-public class Preensambre {
+public class Preensamble {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         boolean repetir = true; // inicializamos repetir como true
-
+        double subtotal = 0; // inicializamos el subtotal en 0
+        
         //----------------------------------Procesadores
         //-------------------------------------------------------------------------------INTEL
         String[] marcaCPU = {"INTEL","AMD"};
         String[] intelGenCPU = {"Intel Core 12th", "Intel Core 13th", "Intel Core 14th"};
-        double[] whattsintel = {100, 200, 300};
+        double[] whattsintel = { 100, 200, 300};
+        //-------------------------------------------------------------------------------Generacion CPU
         String[] intel12thCPU = {"Intel Core i3 12100", "Intel Core i5 12600", "Intel Core i7 12700", "Intel Core i9 12900"};
-        double [] preciosintel12th = {2299, 2399, 5169};
-        double[] whattsintel12th = {100, 200, 300, 400};
+        double[] whattsintel12th = { 100, 200, 300, 400};
+        double[] preciosintel12th = { 2299, 2399, 5169, 6918};
         String[] intel13thCPU = {"Intel Core i3 13100", "Intel Core i5 13600", "Intel Core i7 13700", "Intel Core i9 13900"};
-        double [] preciosintel13th = { 6918, 2209, 3854, 4904};
-        double[] whattsintel13th = {100, 200, 300, 400};
+        double[] whattsintel13th = { 100, 200, 300, 400};
+        double[] preciosintel13th = { 2209, 3854, 4904, 11530};
         String[] intel14thCPU = {"Intel Core i3 14100", "Intel Core i5 14600", "Intel Core i7 14700", "Intel Core i9 14900"};
-        double [] preciosintel14th = {11530, 2259, 5013, 4904};
-        double[] whattsintel14th = {100, 200, 300, 400};
+        double[] whattsintel14th = { 100, 200, 300, 400};
+        double[] preciosintel14th = { 2259, 5013, 4904, 12059};
         //--------------------------------------------------------------------------AMD
         String[] amdGenCPU = {"AMD Ryzen 3000", "AMD Ryzen 5000", "AMD Ryzen 7000"};
-        double[] whattsamd = {100, 200, 300};
+        double[] whattsamd = { 100, 200, 300};
+        //-------------------------------------------------------------------------------Generacion CPU
         String[] amd3000CPU = {"AMD Ryzen 3 3100", "AMD Ryzen 5 3600", "AMD Ryzen 7 3700X", "AMD Ryzen 9 3900X"};
-        double [] preciosamd3000 = {2299, 2399, 5169};
-        double[] whattsamd3000 = {100, 200, 300, 400};
+        double[] whattsamd3000 = { 100, 200, 300, 400};
+        double[] preciosamd3000 = { 1359, 2447, 4906, 13402}; 
         String[] amd5000CPU = {"AMD Ryzen 3 5100", "AMD Ryzen 5 5600", "AMD Ryzen 7 5700X", "AMD Ryzen 9 5900X"};
-        double [] preciosamd5000 = {2299, 2399, 5169};
-        double[] whattsamd5000 = {100, 200, 300, 400};
+        double[] whattsamd5000 = { 100, 200, 300, 400};
+        double[] preciosamd5000 = { 1500, 2880, 3435, 5741};
         String[] amd7000CPU = {"AMD Ryzen 3 7100", "AMD Ryzen 5 7600", "AMD Ryzen 7 7700X", "AMD Ryzen 9 7900X"};
-        double [] preciosamd7000 = {2299, 2399, 5169};
-        double[] whattsamd7000 = {100, 200, 300, 400};
-
+        double[] whattsamd7000 = { 100, 200, 300, 400};
+        double[] preciosamd7000 = { 2259, 5013, 4904, 12059};
+        
         while (repetir) {
-            System.out.println("Binevenido a sistema de armado guiado de PC de ALRM-TECH");
+            System.out.println("Bienvenido a sistema de armado guiado de PC de ALRM-TECH");
             System.out.println();
             System.out.println("¿Qué desea comprar?");
             System.out.println();
@@ -54,7 +57,6 @@ public class Preensambre {
                 scanner.nextLine(); // Limpiar la cache
                 continue;
             }
-
             switch (opcion) {
                 case 1:
                     System.out.println("Ha seleccionado INTEL");
@@ -71,32 +73,47 @@ public class Preensambre {
                     switch (opcionIntel) {
                         case 1:
                             System.out.println("Ha seleccionado Intel Core 12th");
-                            System.out.println("Seleccione la gama del procesador");
+                            System.out.println("Seleccione la gama del procesador:");
                             for (int i = 0; i < intel12thCPU.length; i++) {
-                                System.out.println((i + 1) + ". " + intel12thCPU[i]);
+                                System.out.println((i + 1) + ". " + intel12thCPU[i] + " $" + preciosintel12th[i]);
                             }
-                            // Agregar más opciones aquí si es necesario
+                            System.out.println();
+                            System.out.print("Ingrese su opción: ");
+                            int opcionIntel12th = scanner.nextInt();
+                            scanner.nextLine();
+                            subtotal += preciosintel12th[opcionIntel12th - 1]; // Corregir el índice aquí
+                            System.out.println(subtotal);
                             break;
                         case 2:
                             System.out.println("Ha seleccionado Intel Core 13th");
                             System.out.println("Seleccione la gama del procesador:");
                             for (int i = 0; i < intel13thCPU.length; i++) {
-                                System.out.println((i + 1) + ". " + intel13thCPU[i]);
+                                System.out.println((i + 1) + ". " + intel13thCPU[i] + " $" + preciosintel13th[i]);
                             }
-                            // Agregar más opciones aquí si es necesario
+                            System.out.println();
+                            System.out.print("Ingrese su opción: ");
+                            int opcionIntel13th = scanner.nextInt();
+                            scanner.nextLine();
+                            subtotal += preciosintel13th[opcionIntel13th - 1]; // Corregir el índice aquí
+                            System.out.println(subtotal);
                             break;
                         case 3:
                             System.out.println("Ha seleccionado Intel Core 14th");
                             System.out.println("Seleccione la gama del procesador:");
                             for (int i = 0; i < intel14thCPU.length; i++) {
-                                System.out.println((i + 1) + ". " + intel14thCPU[i]);
+                                System.out.println((i + 1) + ". " + intel14thCPU[i] + " $" + preciosintel14th[i]);
                             }
-                            // Agregar más opciones aquí si es necesario
+                            System.out.println();
+                            System.out.print("Ingrese su opción: ");
+                            int opcionIntel14th = scanner.nextInt();
+                            scanner.nextLine();
+                            subtotal += preciosintel14th[opcionIntel14th - 1]; // Corregir el índice aquí
+                            System.out.println(subtotal);
                             break;
                         default:
                             System.out.println("Opción inválida. Inténtelo de nuevo.");
                             break;
-                    }
+                    }                    
                     break;
                 case 2:
                     System.out.println("Ha seleccionado AMD");
